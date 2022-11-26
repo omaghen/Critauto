e0a0a5c13581d7ccf223d8380f9fd4cba8426e49
Nouhaila Khouna <nouhaila.khouna@univ-reims.fr>
2022-11-26 02:53:24 +0100

    Update

--- main.py
+++ main.py
@@ -1,39 +1,117 @@
-from tools import import_bdd_excel, supression_NA, XX, rég_log, catégorie_émission_voiture
+from tools import import_bdd_csv, XX, splitting_train_test, rég_log, ADL, Random_forest, catégorie_émission_voiture_rég_log, catégorie_émission_voiture_random_forest
+import statistics
 
 if __name__ == '__main__':
-    path = 'C:/Users/b/Documents/Projet CO2'    # chemin où se trouve le jeu de données
+
+    path = 'C:/Users/b/Documents/Projet CO2'  # chemin où se trouve le jeu de données
 
     # Importation du jeu de données (nettoyé)
-    data = import_bdd_excel(path, 'new_CO2_2')
+    data = import_bdd_csv(path, 'Complete')
     data.head()
 
     # Remplacement des variables catégorielles en variables quantitatives
-    liste = ['conso_urb', 'conso_exurb', 'cod_cbr', 'lib_mrq', 'typ_boite_nb_rapp', 'gamme']
-    data = XX(data, liste)
+    data = XX(data)
 
-    # Demande des variables optimales sélectionnées par les algorithmes de sélection de modèles
+    # Demander la saisie des variables optimales sélectionnées par les algorithmes de sélection de modèles
     print('Veuillez saisir la liste des variables optimales sélectionnées par les algorithmes de sélection de modèles ')
     liste_var_opt = input()
 
+    #liste_var_opt = ['conso_urb', 'conso_exurb', 'FE', 'puiss_admin_98', 'TS TERRAINS/CHEMINS', 'GO']
+
+    #Création des données d'apprentissage et de test
+    X_train = splitting_train_test(data, liste_var_opt)[0]
+    y_train = splitting_train_test(data, liste_var_opt)[1]
+    X_test = splitting_train_test(data, liste_var_opt)[2]
+    y_test = splitting_train_test(data, liste_var_opt)[3]
+
+    '''1st model: logistic regression'''
+    #Les coefficients de la régression logistique
+    coeff_reg_log = rég_log(data, X_train, y_train, X_test, y_test)[0]
+
+    #La matrice de confusion de la régression logisitique
+    matrice_confusion_reg_log = rég_log(data, X_train, y_train, X_test, y_test)[1]
+    print("La matrice de confusion de la régression logistique : \n", matrice_confusion_reg_log)
+
+    '''2nd model : ADL'''
     # Les coefficients de la régression logistique
-    coeff_reg_log = rég_log(data, liste_var_opt)[0]
-    print(coeff_reg_log)
+    coeff_adl = rég_log(data, X_train, y_train, X_test, y_test)[0]
 
-    # La matrice de confusion de la régression logisitique
-    matrice_de_confusion = rég_log(data, liste_var_opt)[1]
-    print(matrice_de_confusion)
+    # La matrice de confusion de l'ADL ( = 0.05917576611482922)
+    matrice_conf_ADL = ADL(X_train, y_train, X_test, y_test)[1]
+    print("La matrice de confusion de l'ADL : \n", matrice_conf_ADL)
 
-    # Catégorie d'émission d'un véhicule (catégorie : Forte, faible ou moyenne)
+
+    '''3rd model : Random forest'''
+    # La matrice de confusion de Random Forest
+    matrice_conf_random_forest = Random_forest(X_train, y_train, X_test, y_test)[1]
+    print("La matrice de confusion de Random Forest: \n", matrice_conf_random_forest)
+
+
+
+    '''Comparaison des modèles: Erreurs de classification'''
+    M = 100
+    Erreur_reg_log = [0] * M
+    Erreur_ADL = [0] * M
+    Erreur_random_forest = [0] * M
+    for i in range(0, M):
+        # Création des données d'apprentissage et de test
+        X_train = splitting_train_test(data, liste_var_opt)[0]
+        y_train = splitting_train_test(data, liste_var_opt)[1]
+        X_test = splitting_train_test(data, liste_var_opt)[2]
+        y_test = splitting_train_test(data, liste_var_opt)[3]
+
+        '''1st model: logistic regression'''
+        # Erreur de classification de la régression logisitique ( = 0.008805917576611533)
+        Erreur_reg_log[i] = rég_log(data, X_train, y_train, X_test, y_test)[2]
+
+        '''2nd model : ADL'''
+        # Erreur de classification de l'ADL ( = 0.002465656921451176)
+        Erreur_ADL[i] = ADL(X_train, y_train, X_test, y_test)[2]
+
+        '''3rd model : Random forest'''
+        # Erreur de classification de Random Forest
+        Erreur_random_forest[i] = Random_forest(X_train, y_train, X_test, y_test)[2]
+
+    Erreur_classif_reg_log = statistics.mean([Erreur_reg_log])
+    print("L'erreur de classification de la régression logistique est ", Erreur_classif_reg_log)
+
+    Erreur_classif_ADL = statistics.mean([Erreur_ADL])
+    print("L'erreur de classification de l'ADL est ", Erreur_classif_ADL)
+
+    Erreur_classif_random_forest = statistics.mean([Erreur_random_forest])
+    print("L'erreur de classification de Random Forest est ", Erreur_classif_random_forest)
+
+    Erreur_minim = min(Erreur_classif_reg_log, Erreur_classif_ADL, Erreur_classif_random_forest)
+
+    if Erreur_minim == Erreur_classif_reg_log:
+        modele_optimal = "la régression logistique"
+    elif Erreur_minim == Erreur_classif_ADL:
+        modele_optimal = "l'ADL"
+    else:
+        modele_optimal = "le Random Forest"
+
+    print('Le modèle qui classifie le mieux les véhicules est ', modele_optimal )
+
+    '''Exemple classification d'un vehicule selon la régression logistique'''
+
+    statut_pollution_voiture_random_forest = catégorie_émission_voiture_random_forest(X_train, y_train, X_test, y_test,
+                                                                                      puiss_admin_98, conso_urb,
+                                                                                      conso_exurb,
+                                                                                      cod_cbrFE, cod_cbrGO,
+                                                                                      carrosserieTS_TERRAINS_CHEMINS)
+    print("Selon le Random Forest, ce véhicule a une", statut_pollution_voiture_random_forest, "de CO2")
+    
+
+    '''Exemple classification d'un vehicule selon la régression logistique'''
+    puiss_admin_98 = 100
     conso_urb = 15
     conso_exurb = 7
-    cod_cbrGO = 0  # Attention si 1 GO alors 0 FE
-    lib_mrqMERCEDES = 0
     cod_cbrFE = 1
-    gammeMOY_INFER = 1
-    lib_mrqLEXUS = 1  # Attention si 1 alors aux autres !
-    typ_boite_nb_rappA_6 = 0
+    cod_cbrGO = 0  # Attention si 1 GO alors 0 FE
+    carrosserieTS_TERRAINS_CHEMINS = 1
 
-    statut_pollution_voiture = catégorie_émission_voiture(coeff_reg_log, conso_urb, conso_exurb, cod_cbrGO,
-                                                          lib_mrqMERCEDES, cod_cbrFE, gammeMOY_INFER, lib_mrqLEXUS,
-                                                          typ_boite_nb_rappA_6)
-    print(statut_pollution_voiture)
+    statut_pollution_voiture_reg_log = catégorie_émission_voiture_rég_log(coeff_reg_log, puiss_admin_98, conso_urb, conso_exurb,
+                                                                  cod_cbrFE, cod_cbrGO, carrosserieTS_TERRAINS_CHEMINS)
+    print("Selon la régression logistique, ce véhicule a une", statut_pollution_voiture_reg_log, "de CO2")
+
+
--- tools.py
+++ tools.py
@@ -1,10 +1,14 @@
 from pylab import*
 import pandas as pd
-import random
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import MultiTaskLassoCV
 
 '''Importation du jeu de donnees'''
 def import_bdd_csv(path, nom_bdd):
@@ -58,16 +62,40 @@ def nombre_colonnes(nom_bdd):
 
 
 '''Remplacement chacune des variables qualitatives par les indicatrices de ses modalités'''
-# liste_var = contient les noms des variables quali à remplacer par des indicatrices
-def XX(data, liste):
+def XX(data):
+    liste = data.columns
     n = len(liste)      #nombre des varibles à convertir
-    for i in range(0,n):
+    for i in range(0,n):        
         if (data[liste[i]].dtype == dtype('O')) & ((data[liste[i]].name) != 'var_co2'):     #type = object (qualitative)
             dummies = pd.get_dummies(data[liste[i]])
-            #dummies = dummies.drop([dummies.columns[0]], axis=1)
-            #data = data.drop([liste[i]], axis=1)
+            data = data.drop([liste[i]], axis=1)
             data = pd.concat([data, dummies], axis=1)
     return data
+
+
+''' Régression Lasso pour la sélection des variables'''
+def reg_LASSO(data):
+    data['var_co2'] = data['var_co2'].astype('category')
+    df3 = data.select_dtypes(include=np.number)  # sélectionne toutes les variables numériques (features)
+    df3.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df3 = df3.fillna(0)
+    scaler = StandardScaler()
+    yindex = data['var_co2'].cat.codes  # target
+    df3_scale = scaler.fit(df3).transform(df3)
+    X_train, X_test , y_train, y_test = train_test_split(np.delete(data, yindex, axis = 1),data[:,yindex], test_size=0.2, random_state=0)
+    my_alphas = [0.001, 0.01, 0.02, 0.025, 0.05, 0.1, 0.25, 0.5, 0.8, 1.0]
+    lcv = MultiTaskLassoCV(alphas=my_alphas, normalize=False, fit_intercept=False, random_state=0, cv=5)
+    lcv.fit(np.delete(df3_scale, yindex, axis=1), df3_scale[:, yindex])
+    # Alpha optimal
+    print("alpha optimale : ", lcv.alpha_)
+    lasso2 = Lasso(fit_intercept=True, alpha=lcv.alpha_).fit(X_train, y_train)
+    features_selec2 = data.select_dtypes(include=np.number).drop("var_co2", axis=1).columns[
+        np.abs(lasso2.coef_) > 0].tolist()
+    print(features_selec2)
+    # nombre de variables sélectionnées
+    nlasso = sum(np.abs(lasso2.coef_) > 0)
+
+    return features_selec2
 
 '''Fractionnement des données d'entraînement et de test'''
 def splitting_train_test(data, liste_var):
@@ -76,7 +104,7 @@ def splitting_train_test(data, liste_var):
     for i in range(0, k):
         if (data.columns[i] not in liste_var) & (data.columns[i] != 'var_co2'):
             p = data.columns[i]
-            d = d.drop([p], axis=1)  # pour garder que les var du modèle optimal
+            d = d.drop([p], axis=1)  # pour ne garder que les variables du modèle optimal
             k = k - 1
     data = d
     # Creating the features and target
@@ -84,21 +112,18 @@ def splitting_train_test(data, liste_var):
     features = data.select_dtypes(np.number)  # pour sélectionner que les variables de type numérique
     target = data['var_co2'].cat.codes  # Return Series of codes as well as the index: [0 = faible], [1 = forte], [2 = moyenne]
     # Creating the training and testing data
-    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size = 0.33, random_state = 42, stratify = target)
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2619, random_state=42, stratify=target)
 
     return X_train, y_train, X_test, y_test
 
 
 ''' régression logistique sur le modèle optimal '''
 def rég_log(data, X_train, y_train, X_test, y_test):
-    data['var_co2'] = data['var_co2'].astype('category')
-    features = data.select_dtypes(np.number)  # pour sélectionner que les variables de type numérique
+    features = X_train.select_dtypes(np.number)
     # Initializing an logistic regression object
-    logistic_reg = linear_model.LogisticRegression()
+    logistic_reg = linear_model.LogisticRegression(solver='lbfgs', max_iter=1000)
     # Fitting the model to the training and test sets
     logistic_reg.fit(X_train, y_train)
-    # Accuracy score of the logistic regression model
-    logistic_reg.score(X_test, y_test)  # précision du modèle #on obtient un score entre 0 et 1 (0.95 = 95%)
 
     logistic_reg.intercept_
     logistic_reg.coef_
@@ -108,18 +133,22 @@ def rég_log(data, X_train, y_train, X_test, y_test):
                                  index=["Faible", "Forte", "Moyenne"],
                                  columns=["intercept"] + list(features.columns)).T
 
-    #calcul de la matrice de confusion
+    # Accuracy score of the logistic regression model
+    accuracy = logistic_reg.score(X_test, y_test)  # précision du modèle
+    Erreur_classif = 1 - accuracy
+
+    #matrice de confusion
     y_pred = []
     for i in range(len(X_test)):
         pred = int(logistic_reg.predict([np.array(X_test)[i]]))
         y_pred.append(pred)
     matrice = confusion_matrix(y_true=y_test, y_pred=y_pred)
 
-    return coeff_reg_log, matrice
-
+    return coeff_reg_log, matrice, Erreur_classif
 
 '''Analyse discriminante linéaire'''
 def ADL(X_train, y_train, X_test, y_test):
+    features = X_train.select_dtypes(np.number)
     # Initializing an discriminant analysis object
     adl = LinearDiscriminantAnalysis()
     # Fitting the model to the training and test sets
@@ -130,10 +159,14 @@ def ADL(X_train, y_train, X_test, y_test):
     adl.intercept_
     adl.coef_
 
-    # Tableau variables et leurs coefficents de la régression logistique
+    # Tableau variables et leurs coefficents de l'ADL
     coeff_adl = pd.DataFrame(np.concatenate([adl.intercept_.reshape(-1, 1), adl.coef_], axis=1),
                              index=["Faible", "Forte", "Moyenne"],
                              columns=["intercept"] + list(features.columns)).T
+
+    # Accuracy score of the logistic regression model
+    accuracy = adl.score(X_test, y_test)  # précision du modèle
+    Erreur_classif = 1 - accuracy
 
     # matrice de confusion
     y_pred = []
@@ -143,7 +176,7 @@ def ADL(X_train, y_train, X_test, y_test):
 
     matrice = confusion_matrix(y_true=y_test, y_pred=y_pred)
 
-    return coeff_adl, matrice
+    return coeff_adl, matrice, Erreur_classif
 
 '''Random Forest '''
 def Random_forest(X_train, y_train, X_test, y_test):
@@ -167,34 +200,34 @@ def Random_forest(X_train, y_train, X_test, y_test):
         ccp_alpha=0.0,
         max_samples=None, )
     # Apprentissage
-    model_rf.fit(X_train, y_train)
+    modele_rf.fit(X_train, y_train)
 
     # l'importance des variables de notre modèle de foret aléatoire
     classement_var = pd.DataFrame(modele_rf.feature_importances_,
-                                  index=x_train.columns,
+                                  index=X_train.columns,
                                   columns=["importance"]).sort_values("importance", ascending=False)
-    # Validation du modèle et prédiction
-    # accuracy
-    print(f"Le pourcentage de bien classés est de : {accuracy_score(y_test, modele_rf.predict(x_test)) * 100} %")
+    # Accuracy
+    Accuracy_random_forest = accuracy_score(y_test, modele_rf.predict(X_test))
+    Erreur_classif = 1 - Accuracy_random_forest
+    # print(f"Le pourcentage de bien classés est de : {accuracy_score(y_test, modele_rf.predict(X_test))*100} %")
 
     # Matrice de confusion (pour avoir une analyse plus fine des résultats)
-    matrice = pd.DataFrame(confusion_matrix(y_test, modele_rf.predict(x_test)),
-                           index=["blanc_données", "rouge_données"],
-                           columns=["blanc_predit", "rouge_predit"])
-    return classement_var, matrice
+    matrice = pd.DataFrame(confusion_matrix(y_test, modele_rf.predict(X_test)),
+                           index=["Faible_donnees", "Forte_donnees", "Moyenne_donnees"],
+                           columns=["Faible_predit", "Forte_predit", "Moyenne_predit"])
 
-'''Catégorie d'émission de la voiture: Forte, faible ou moyenne'''
-def catégorie_émission_voiture(coeff_reg_log, conso_urb, conso_exurb, cod_cbrGO, lib_mrqMERCEDES, cod_cbrFE,
-                               gammeMOY_INFER, lib_mrqLEXUS, typ_boite_nb_rappA_6):
+    return classement_var, matrice, Erreur_classif
+
+
+'''Classification des voitures selon le modèle de régression logistique'''
+def catégorie_émission_voiture_rég_log(coeff_reg_log, puiss_admin_98, conso_urb, conso_exurb, cod_cbrFE, cod_cbrGO,carrosserieTS_TERRAINS_CHEMINS):
     f = coeff_reg_log['Forte']
-    forte = f[0] + f[1] * conso_urb + f[2] * conso_exurb + f[33] * cod_cbrGO + [46] * gammeMOY_INFER + f[
-        15] * lib_mrqLEXUS + f[35] * typ_boite_nb_rappA_6
-    # f[3]*lib_mrqMERCEDES + f[]*cod_cbrFE +
+    forte = f[0] + f[1] * puiss_admin_98 + f[2] * conso_urb + f[3] * conso_exurb + f[4] * cod_cbrFE + f[5] * cod_cbrGO + \
+            f[6] * carrosserieTS_TERRAINS_CHEMINS
 
     m = coeff_reg_log['Moyenne']
-    moyenne = m[0] + m[1] * conso_urb + m[2] * conso_exurb + m[31] * cod_cbrGO + m[46] * gammeMOY_INFER + m[
-        15] * lib_mrqLEXUS + m[35] * typ_boite_nb_rappA_6
-    # 'm[3]*lib_mrqMERCEDES + m[]*cod_cbrFE +'
+    moyenne = m[0] + m[1] * puiss_admin_98 + m[2] * conso_urb + m[3] * conso_exurb + m[4] * cod_cbrFE + m[
+        5] * cod_cbrGO + m[6] * carrosserieTS_TERRAINS_CHEMINS
 
     Pfaible = 1 / (1 + exp(forte) + exp(moyenne))  # proba Y=faible conditionnellement au var explicaive
     Pforte = exp(forte) / (1 + exp(forte) + exp(moyenne))  # proba Y=forte conditionnellement au var explicaive
@@ -209,5 +242,21 @@ def catégorie_émission_voiture(coeff_reg_log, conso_urb, conso_exurb, cod_cbrG
         catégorie = "Forte émission"
     else:
         catégorie = "moyenne émission"
+
+    return catégorie
+
+'''Classification des voitures selon le modèle de Random Forest'''
+def catégorie_émission_voiture_random_forest(X_train, y_train, X_test, y_test, puiss_admin_98, conso_urb, conso_exurb,
+                                             cod_cbrFE, cod_cbrGO, carrosserieTS_TERRAINS_CHEMINS):
+    X_new_car = [puiss_admin_98, conso_urb, conso_exurb, cod_cbrFE, cod_cbrGO, carrosserieTS_TERRAINS_CHEMINS]
+    modele_rf = Random_forest(X_train, y_train, X_test, y_test)[0]
+    m = int(modele_rf.predict([X_new_car]))
+
+    if m == 0:
+        catégorie = "Faible émission"
+    elif m == 1:
+        catégorie = "Forte émission"
+    else:
+        catégorie = "Moyenne émission"
 
     return catégorie
